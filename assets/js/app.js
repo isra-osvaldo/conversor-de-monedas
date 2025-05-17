8fe11003ba50fc5ed873970696f88f14cb62aa26
isra-osvaldo <israel.acuna7@gmail.com>
2025-05-17 01:09:22 -0400

    Refactor currency conversion logic to simplify API calls and improve readability

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,61 +1,33 @@
 const $btnConvertir = document.querySelector('#convertir')
 const $resultado = document.querySelector('#resultado')
-const $selectMoneda = document.getElementById('moneda')
 const $valorCLP = (document.getElementById('monto'))
-
-
-async function getMonedas() {
-    try {
-        const dolar = await fetch('https://mindicador.cl/api/dolar')
-        const euro = await fetch('https://mindicador.cl/api/euro')
-        
-        const dataDolar = await dolar.json()
-        const dataEuro = await euro.json()
-
-        const valorActualDolar = dataDolar.serie[0].valor
-        const valorActualEuro = dataEuro.serie[0].valor
-
-        return { valorActualDolar, valorActualEuro }
-
-    } catch (error) {
-        console.error('Error al obtener los datos:', error) 
-    }
-}
-
-function calcularConversion(valorCLP, valorMoneda) {
-    return valorCLP / valorMoneda
-}
 
 function mostrarResultado(mensaje) {
     $resultado.innerHTML = mensaje
 }
 
-
-async function convertirMoneda() {
-    const { valorActualDolar, valorActualEuro } = await getMonedas()
-    const valorDolar = valorActualDolar
-    const valorEuro = valorActualEuro
-    
-    $btnConvertir.addEventListener('click', () => {
-        const montoCLP = Number($valorCLP.value)
-        const moneda = $selectMoneda.value
-        let result = 0
+async function getMonedas(moneda) {
+    const selectMoneda = document.getElementById('moneda').value
+    try {
+        const res = await fetch(`https://mindicador.cl/api/${moneda}`)
+        const data = await res.json()        
+        const valorActual = data.serie[0].valor
+        return valorActual
         
-        if (moneda === 'dolar') {
-            result = calcularConversion(montoCLP, valorDolar)
-            mostrarResultado(`Resultado: $${result.toFixed(2)} USD`)
-        }
-        else if (moneda === 'euro') {
-            result = calcularConversion(montoCLP, valorEuro)
-            mostrarResultado(`Resultado: $${result.toFixed(2)} € `)
-        }
-        else {
-            mostrarResultado('Seleccione una moneda válida')
-        }
-
-        $valorCLP.value = ''
-
-    })
+    } catch (error) {
+        console.error('Error al obtener los datos:', error) 
+    }
 }
 
-convertirMoneda()
+$btnConvertir.addEventListener('click', async () => {
+    const selectMoneda = document.getElementById('moneda').value
+    const valorActual = await getMonedas(selectMoneda)
+    const montoCLP = Number($valorCLP.value)
+    monedaConvertida = (montoCLP / valorActual).toFixed(2)
+
+    const simbol = selectMoneda === 'dolar' ? 'USD' : '€' 
+    mostrarResultado(`Resultado: ${monedaConvertida} ${simbol}`)
+    $valorCLP.value = ''
+
+    })
+
